7eb3a665f3656d360c49a197f4daf937222bb1a3
市原 <s1942008ua@s.chibakoudai.jp>
2021-07-06 17:25:18 +0900

    hello

--- mydb.sql
+++ mydb.sql
@@ -1,8 +1,8 @@
--- MariaDB dump 10.19  Distrib 10.4.19-MariaDB, for Win64 (AMD64)
+-- MariaDB dump 10.19  Distrib 10.4.18-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: mydb
 -- ------------------------------------------------------
--- Server version	10.4.19-MariaDB
+-- Server version	10.4.18-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,29 +16,77 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `table1`
+-- Table structure for table `new`
 --
 
-DROP TABLE IF EXISTS `table1`;
+DROP TABLE IF EXISTS `new`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `table1` (
+CREATE TABLE `new` (
+  `mytabel` int(20) NOT NULL,
+  `table1` int(20) NOT NULL,
+  UNIQUE KEY `mytabel` (`mytabel`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `new`
+--
+
+LOCK TABLES `new` WRITE;
+/*!40000 ALTER TABLE `new` DISABLE KEYS */;
+/*!40000 ALTER TABLE `new` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productinfo`
+--
+
+DROP TABLE IF EXISTS `productinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `productinfo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `varcharA` varchar(40) NOT NULL,
-  `intA` int(11) NOT NULL,
-  `intB` int(11) NOT NULL,
+  `productname` varchar(30) NOT NULL,
+  `price` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `table1`
+-- Dumping data for table `productinfo`
 --
 
-LOCK TABLES `table1` WRITE;
-/*!40000 ALTER TABLE `table1` DISABLE KEYS */;
-INSERT INTO `table1` VALUES (1,'A',1280,1),(2,'B',2980,0),(3,'C',198,121);
-/*!40000 ALTER TABLE `table1` ENABLE KEYS */;
+LOCK TABLES `productinfo` WRITE;
+/*!40000 ALTER TABLE `productinfo` DISABLE KEYS */;
+INSERT INTO `productinfo` VALUES (1,'イヤホン',100,1),(2,'モバイルバッテリ',2980,1),(3,'ライター',198,121);
+/*!40000 ALTER TABLE `productinfo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userinfo`
+--
+
+DROP TABLE IF EXISTS `userinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `userinfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `passwd` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userinfo`
+--
+
+LOCK TABLES `userinfo` WRITE;
+/*!40000 ALTER TABLE `userinfo` DISABLE KEYS */;
+INSERT INTO `userinfo` VALUES (1,'0608','0913'),(2,'user2','pass2'),(3,'admin','pass3');
+/*!40000 ALTER TABLE `userinfo` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -50,4 +98,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-03 10:34:42
+-- Dump completed on 2021-07-06 17:24:18
