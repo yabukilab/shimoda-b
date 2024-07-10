9f6ecab47d002c8cbc335dc5c3cfdc938b8b2732
Kayu0401 <>
2024-07-11 07:14:13 +0900

    ddd

--- htdocs/edit_seat.php
+++ htdocs/edit_seat.php
@@ -6,6 +6,12 @@
     <link rel="stylesheet" href="seat_styles.css">
 </head>
 <body>
+
+    <div>
+        <h1>利用する座席ボタンを押してください</h1>
+        <h1>利用後にもう一度座席ボタンを押してください</h1>
+    </div> 
+
     <div class="seat-container">
         <!-- 座席ボタンをJavaScriptで動的に生成 -->
     </div>
