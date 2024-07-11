abeb9ddf0d010db000996b1afd335c8d2c51f299
Kayu0401 <>
2024-07-11 15:09:44 +0900

    aaaa

--- htdocs/index.php
+++ htdocs/index.php
@@ -12,7 +12,7 @@
 
         <h1>本日のメニュー</h1>
         <form action="disp_seat.php" method="post">
-        <button type="submit" name="seat">混雑状況</button>
+        <button type="submit" name="seat">混雑<br>状況</button>
 
         <?php
 
