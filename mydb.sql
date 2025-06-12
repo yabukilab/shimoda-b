ea19e51ff880701ab58308e87af55a7f384ac10d
安藤 <s2342010lc@s.chibakoudai.jp>
2025-06-12 13:50:18 +0900

    2025/06/12

--- mydb.sql
+++ mydb.sql
@@ -1,6 +1,6 @@
 -- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
 --
--- Host: localhost    Database: study5
+-- Host: localhost    Database: payment_search
 -- ------------------------------------------------------
 -- Server version	10.4.32-MariaDB
 
@@ -16,29 +16,34 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `mst_product`
+-- Table structure for table `stores`
 --
 
-DROP TABLE IF EXISTS `mst_product`;
+DROP TABLE IF EXISTS `stores`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `mst_product` (
-  `code` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
-  `price` int(11) NOT NULL,
-  `comment` varchar(200) NOT NULL,
-  PRIMARY KEY (`code`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE `stores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `store_name` varchar(255) NOT NULL,
+  `area` varchar(255) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `category` varchar(50) NOT NULL,
+  `payment_methods` text NOT NULL,
+  `hours` varchar(255) NOT NULL,
+  `holidays` varchar(255) NOT NULL,
+  `notes` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `mst_product`
+-- Dumping data for table `stores`
 --
 
-LOCK TABLES `mst_product` WRITE;
-/*!40000 ALTER TABLE `mst_product` DISABLE KEYS */;
-INSERT INTO `mst_product` VALUES (1,'パソコン',500,'速い'),(2,'スマホ',1000,'軽い'),(3,'新タブレット',5,'カッコいい');
-/*!40000 ALTER TABLE `mst_product` ENABLE KEYS */;
+LOCK TABLES `stores` WRITE;
+/*!40000 ALTER TABLE `stores` DISABLE KEYS */;
+INSERT INTO `stores` VALUES (1,'カフェさくら','津田沼','千葉県船橋市前原西2-21-1','food','credit,qr,cash','10:00〜20:00','火曜日','自家製スイーツが人気です。'),(2,'グリーンマーケット','習志野','千葉県習志野市津田沼4-8-15','retail','qr,emoney','9:00〜18:00','日曜日','地元野菜を扱っています。'),(3,'リラックス整体院','船橋','千葉県船橋市本町3-12-5','service','credit,emoney','11:00〜22:00','水曜日','完全予約制'),(4,'アジアンレストラン タイサバイ','津田沼','千葉県船橋市前原東3-7-8','food','credit,qr,emoney,cash','11:30〜15:00, 17:00〜22:00','月曜日','英語メニューあり'),(5,'cut純','東京','足立区','service','credit,qr,emoney,cash','10:00~20:00','毎週水曜日','');
+/*!40000 ALTER TABLE `stores` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -50,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-12 10:46:42
+-- Dump completed on 2025-06-12 13:47:01
